340842e7d20585887cdb4f9009f723643ac0a8f2
writemorecode <gustav.ae.karlsson@gmail.com>
2025-03-27 17:44:36 +0100

    Fixed tests

--- tests/lex.rs
+++ tests/lex.rs
@@ -3,14 +3,14 @@ use databas::lex::{Lexer, Token, TokenKind};
 #[test]
 fn test_skip_whitespace() {
     let s = "   (";
-    let token = Token {
+    let mut lexer = Lexer::new(s);
+    let got = lexer.next();
+    let expected = Token {
         kind: TokenKind::LeftParen,
         lexeme: None,
-        offset: s.find('(').unwrap(),
+        offset: 3,
     };
-    let mut lexer = Lexer::new(s);
-    let next = lexer.next().unwrap();
-    assert_eq!(token, next);
+    assert_eq!(Some(Ok(expected)), got);
     assert!(lexer.rest.is_empty());
     assert_eq!(lexer.position, s.len());
 }
@@ -18,15 +18,14 @@ fn test_skip_whitespace() {
 #[test]
 fn test_lex_number() {
     let s = "1234";
-    let token = Token {
+    let mut lexer = Lexer::new(s);
+    let next = lexer.next();
+    let expected = Token {
         kind: TokenKind::Number(1234),
         lexeme: None,
         offset: 0,
     };
-
-    let mut lexer = Lexer::new(s);
-    let next = lexer.next().unwrap();
-    assert_eq!(token, next);
+    assert_eq!(Some(Ok(expected)), next);
     assert!(lexer.rest.is_empty());
     assert_eq!(lexer.position, s.len());
 }
@@ -34,38 +33,29 @@ fn test_lex_number() {
 #[test]
 fn test_lex_number_between_whitespace() {
     let s = " 1234 ";
-    let token = Token {
+    let mut lexer = Lexer::new(s);
+    let got = lexer.next();
+    let expected = Token {
         kind: TokenKind::Number(1234),
         lexeme: None,
         offset: 1,
     };
-
-    let mut lexer = Lexer::new(s);
-    let next = lexer.next().unwrap();
-    assert_eq!(token, next);
+    assert_eq!(Some(Ok(expected)), got);
     assert_eq!(lexer.rest, " ");
     assert_eq!(lexer.position, s.len() - 1);
 }
-
-// #[test]
-// fn test_big() {
-//     let s = "12 + 23 * (36 / 8)";
-//     let lexer = Lexer::new(s);
-//     let tokens: Vec<Token> = lexer.into_iter().collect();
-//     dbg!(tokens);
-// }
 
 #[test]
 fn test_string() {
     let s = r#""hello world""#;
     let mut lexer = Lexer::new(s);
-    let token = lexer.next().unwrap();
+    let got = lexer.next();
     let expected = Token {
         kind: TokenKind::String("hello world"),
         lexeme: None,
         offset: 0,
     };
-    assert_eq!(expected, token);
+    assert_eq!(Some(Ok(expected)), got);
 }
 
 #[test]
@@ -104,4 +94,29 @@ fn test_keywords() {
         let got = lexer.next();
         assert_eq!(got, Some(Ok(t)));
     }
+}
+
+#[test]
+fn test_expression() {
+    let s = "12 + 23 * (36 / 8)";
+    let mut lexer = Lexer::new(s);
+
+    let mut expect = |kind: TokenKind, offset: usize| {
+        let expected = Token {
+            kind,
+            lexeme: None,
+            offset,
+        };
+        let got = lexer.next();
+        assert_eq!(got, Some(Ok(expected)));
+    };
+
+    expect(TokenKind::Number(12), 0);
+    expect(TokenKind::Plus, 3);
+    expect(TokenKind::Number(23), 5);
+    expect(TokenKind::Asterisk, 8);
+    expect(TokenKind::LeftParen, 10);
+    expect(TokenKind::Number(36), 11);
+    expect(TokenKind::Slash, 14);
+    expect(TokenKind::Number(8), 16);
 }
