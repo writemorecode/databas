2e5a642dc2b4f650518e978857b61cf4b8774cf4
writemorecode <gustav.ae.karlsson@gmail.com>
2026-02-20 23:01:21 +0100

    First commit for storage engine
    Started work on disk manager

---
+++ crates/databas_core/src/error.rs
@@ -0,0 +1,13 @@
+pub enum StorageError {
+    IO(std::io::Error),
+    InvalidPageId(u64),
+    InvalidFileSize(u64),
+}
+
+pub(crate) type StorageResult<T> = Result<T, StorageError>;
+
+impl From<std::io::Error> for StorageError {
+    fn from(err: std::io::Error) -> Self {
+        Self::IO(err)
+    }
+}
